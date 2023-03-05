9939cbb6057f57cb248cf9bc57787d4c05d7e837
Facudeandres <115202933+Facudeandres@users.noreply.github.com>
2023-03-05 19:46:28 -0300

    Update columnsandcsv.py

--- columnsandcsv.py
+++ columnsandcsv.py
@@ -3,33 +3,47 @@ import numpy as np
 import streamlit as st
 import re
 
-st.header(":rainbow::rainbow: La peor App de la historia :rainbow::rainbow:")
+# Tamaño del cuadro de texto
+TEXT_BOX_HEIGHT = 5
+TEXT_BOX_WIDTH = 7
 
-convertidor_coma_a_column = "Ingrese texto delimitado por comas"
-convertidor_column_a_coma = "Ingrese texto en columnas"
+# Definir la interfaz
+st.title(":rainbow::rainbow: La peor App de la historia :rainbow::rainbow:")
+st.write("Ingrese texto en uno o ambos de los siguientes cuadros de texto, luego haga clic en el botón 'Convertir Texto'.")
 
-# Dividimos la pantalla en 3 columnas, la primera y la tercera para los inputs y la segunda para el botón
-col1, col2, col3 = st.beta_columns([1, 3, 1])
+# Cuadro de texto para texto delimitado por comas
+st.subheader("Texto delimitado por comas")
+text_input_coma = st.text_input("Ingrese texto delimitado por comas", height=TEXT_BOX_HEIGHT)
 
-# Creamos los inputs dentro de las columnas 1 y 3
-with col1:
-    text_input_coma = st.text_input(convertidor_coma_a_column, height=150)
-with col3:
-    text_input_column = st.text_area(convertidor_column_a_coma, height=150)
+# Cuadro de texto para texto en columnas
+st.subheader("Texto en columnas")
+text_input_column = st.text_area("Ingrese texto en columnas, delimitado por enter", height=TEXT_BOX_HEIGHT, width=TEXT_BOX_WIDTH)
 
-# Botón en el centro de la pantalla
-with col2:
-    if st.button(":hot_pepper: Convertir Texto :hot_pepper:"):
-        if text_input_coma:
-            result = convertidor_coma_a_column(text_input_coma)
+# Funciones de conversión
+def convertidor_coma_a_column(text):
+    if ',' in text:
+        return "\n".join(text.split(", ")).strip()
+    else:
+        raise ValueError(":ghost: Pusiste cualquier cosa Botardo :ghost:")
+
+def convertidor_column_a_coma(text):
+    lines = text.splitlines()
+    if any(',' in line for line in lines):
+        raise ValueError(":ghost: Pusiste cualquier cosa Botardo :ghost:")
+    return ", ".join(lines)
+
+# Botón para convertir texto
+if st.button(":hot_pepper: Convertir Texto :hot_pepper:"):
+    if text_input_coma:
+        result = convertidor_coma_a_column(text_input_coma)
+        st.success("Texto convertido:")
+        st.code(result, language='python')
+    elif text_input_column:
+        try:
+            result = convertidor_column_a_coma(text_input_column)
             st.success("Texto convertido:")
             st.code(result, language='python')
-        elif text_input_column:
-            try:
-                result = convertidor_column_a_coma(text_input_column)
-                st.success("Texto convertido:")
-                st.code(result, language='python')
-            except ValueError as e:
-                st.error(str(e))
-        else:
-            raise ValueError(":ghost: Pusiste cualquier cosa Botardo :ghost:")
+        except ValueError as e:
+            st.error(str(e))
+    else:
+        st.warning("No se ingresó ningún texto.")
