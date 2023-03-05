6fdd6090ddfe84e7efecef2fc5e14fe5c099b08b
Facudeandres <115202933+Facudeandres@users.noreply.github.com>
2023-03-05 19:01:02 -0300

    Update columnsandcsv.py

--- columnsandcsv.py
+++ columnsandcsv.py
@@ -2,25 +2,25 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
-st.header(":rainbow::rainbow: La peor App de la historia :rainbow::rainbow:")
+st.header("La peor App de la historia")
 
-convertidor_coma_a_column = "Ingrese texto delimitado por comas"
-convertidor_column_a_coma = "Ingrese texto en columnas"
+convertidor_coma_a_column = "Ingrese texto separado por comas"
+convertidor_column_a_coma = "Ingrese texto en columnas separadas por salto de línea"
 
 text_input_coma = st.text_input(convertidor_coma_a_column)
 text_input_column = st.text_input(convertidor_column_a_coma)
 
-if st.button(":hot_pepper: Convertir Texto :hot_pepper:"):
+if st.button("Convertir Texto"):
     if text_input_coma != "":
         result = convertidor_coma_a_column(text_input_coma)
         st.success("Texto convertido:")
-        st.code(result, language='python')
+        st.code(result, language='text')
     elif text_input_column != "":
         result = convertidor_column_a_coma(text_input_column)
         st.success("Texto convertido:")
-        st.code(result, language='python')
+        st.code(result, language='text')
     else:
-        raise ValueError(":ghost: Pusiste cualquier cosa Botardo :ghost:")
+        raise ValueError("Pusiste cualquier cosa Botardo")
 
 def convertidor_coma_a_column(text):
     return "\n".join(text.split(", ")).strip()
