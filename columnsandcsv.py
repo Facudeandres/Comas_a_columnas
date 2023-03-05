1632dded610419eb06c2946a362682859d69dd47
Facudeandres <115202933+Facudeandres@users.noreply.github.com>
2023-03-05 19:49:08 -0300

    Update columnsandcsv.py

--- columnsandcsv.py
+++ columnsandcsv.py
@@ -3,23 +3,20 @@ import numpy as np
 import streamlit as st
 import re
 
-# Tamaño del cuadro de texto
-TEXT_BOX_HEIGHT = 5
-TEXT_BOX_WIDTH = 7
+# Definir la constante TEXT_BOX_HEIGHT
+TEXT_BOX_HEIGHT = 5 * 22
 
-# Definir la interfaz
-st.title(":rainbow::rainbow: La peor App de la historia :rainbow::rainbow:")
-st.write("Ingrese texto en uno o ambos de los siguientes cuadros de texto, luego haga clic en el botón 'Convertir Texto'.")
+# Configurar la página
+st.set_page_config(page_title="Conversor de texto",
+                   page_icon=":pencil:",
+                   layout="wide")
 
-# Cuadro de texto para texto delimitado por comas
-st.subheader("Texto delimitado por comas")
-text_input_coma = st.text_input("Ingrese texto delimitado por comas", height=TEXT_BOX_HEIGHT)
+# Definir los cuadros de texto
+st.sidebar.header("Conversor de texto")
+text_input_coma = st.sidebar.text_input("Ingrese texto delimitado por comas", height=TEXT_BOX_HEIGHT)
+text_input_column = st.sidebar.text_area("Ingrese texto en columnas", height=TEXT_BOX_HEIGHT)
 
-# Cuadro de texto para texto en columnas
-st.subheader("Texto en columnas")
-text_input_column = st.text_area("Ingrese texto en columnas, delimitado por enter", height=TEXT_BOX_HEIGHT, width=TEXT_BOX_WIDTH)
-
-# Funciones de conversión
+# Definir las funciones de conversión
 def convertidor_coma_a_column(text):
     if ',' in text:
         return "\n".join(text.split(", ")).strip()
@@ -32,7 +29,7 @@ def convertidor_column_a_coma(text):
         raise ValueError(":ghost: Pusiste cualquier cosa Botardo :ghost:")
     return ", ".join(lines)
 
-# Botón para convertir texto
+# Definir el botón de conversión
 if st.button(":hot_pepper: Convertir Texto :hot_pepper:"):
     if text_input_coma:
         result = convertidor_coma_a_column(text_input_coma)
@@ -46,4 +43,5 @@ if st.button(":hot_pepper: Convertir Texto :hot_pepper:"):
         except ValueError as e:
             st.error(str(e))
     else:
-        st.warning("No se ingresó ningún texto.")
+        raise ValueError(":ghost: Pusiste cualquier cosa Botardo :ghost:")
+
