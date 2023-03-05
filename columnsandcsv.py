4d136e31d1fe1038d709f048d683c4ab30c66283
Facudeandres <115202933+Facudeandres@users.noreply.github.com>
2023-03-05 19:34:36 -0300

    Update columnsandcsv.py

--- columnsandcsv.py
+++ columnsandcsv.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+import re
 
 st.header(":rainbow::rainbow: La peor App de la historia :rainbow::rainbow:")
 
@@ -14,25 +15,26 @@ def convertidor_coma_a_column(text):
     if ',' in text:
         return "\n".join(text.split(", ")).strip()
     else:
-        raise ValueError(":ghost: Pusiste cualquier cosa Botardo :ghost:")
+        st.error(":ghost: Pusiste cualquier cosa Botardo :ghost:")
 
 def convertidor_column_a_coma(text):
-    lines = text.splitlines()
-    if any(',' in line for line in lines):
-        raise ValueError(":ghost: Pusiste cualquier cosa Botardo :ghost:")
-    return ", ".join(lines)
+    lines = text.split('\n')
+    if any(re.search(r',\s*', line) for line in lines):
+        st.error(":ghost: Pusiste cualquier cosa Botardo :ghost:")
+    else:
+        return ", ".join(lines)
 
 if st.button(":hot_pepper: Convertir Texto :hot_pepper:"):
     if text_input_coma:
         result = convertidor_coma_a_column(text_input_coma)
-        st.success("Texto convertido:")
-        st.code(result, language='python')
-    elif text_input_column:
-        try:
-            result = convertidor_column_a_coma(text_input_column)
+        if result:
             st.success("Texto convertido:")
             st.code(result, language='python')
-        except ValueError as e:
-            st.error(str(e))
+    elif text_input_column:
+        result = convertidor_column_a_coma(text_input_column)
+        if result:
+            st.success("Texto convertido:")
+            st.code(result, language='python')
     else:
-        raise ValueError(":ghost: Pusiste cualquier cosa Botardo :ghost:")
+        st.error(":ghost: Pusiste cualquier cosa Botardo :ghost:")
+
